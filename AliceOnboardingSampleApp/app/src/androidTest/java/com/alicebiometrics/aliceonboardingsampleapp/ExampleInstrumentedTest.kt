d67cd266fd1852b5f0dbcd1ee47300113d7c8dac
garciagarciaadrian <agarcia@alicebiometrics.com>
2023-11-06 16:55:00 +0100

    chore: add permissionRuleCamera to test

--- AliceOnboardingSampleApp/app/src/androidTest/java/com/alicebiometrics/aliceonboardingsampleapp/ExampleInstrumentedTest.kt
+++ AliceOnboardingSampleApp/app/src/androidTest/java/com/alicebiometrics/aliceonboardingsampleapp/ExampleInstrumentedTest.kt
@@ -3,44 +3,28 @@ package com.alicebiometrics.aliceonboardingsampleapp
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.runner.AndroidJUnit4
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ExampleInstrumentedTest {
 
-    @Rule @JvmField
-    val mActivityRule = ActivityTestRule(MainActivity::class.java)
+    @get:Rule
+    var intentsRule: IntentsTestRule<MainActivity> = IntentsTestRule(MainActivity::class.java)
 
-    //@get:Rule var permissionRuleCamera = GrantPermissionRule.grant(android.Manifest.permission.CAMERA)
-    //@get:Rule var permissionRuleWriteExternal = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    @get:Rule var permissionRuleCamera = GrantPermissionRule.grant(android.Manifest.permission.CAMERA)
 
-
-    var mLocatingActivity: MainActivity? = null
-
-    @Before
-    fun setup() {
-        mLocatingActivity = mActivityRule.getActivity()
-    }
-
-    @Test fun listGoesOverTheFold() {
+    @Test
+    fun checkButton() {
         onView(withId(R.id.buttonCreateAccount)).check(matches(isDisplayed()))
     }
 }
